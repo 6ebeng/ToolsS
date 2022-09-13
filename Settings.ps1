9339f3525d369aa8e729a8db1bfaf8b8a75e8958
Tishko Rasoul <90611955+6ebeng@users.noreply.github.com>
2022-09-14 01:06:52 +0300

    Update Settings.ps1

--- Settings.ps1
+++ Settings.ps1
@@ -5,7 +5,7 @@ if( $Env:USER1_AUTH_TOKEN -eq $env:UserName) {
 
         new-item $env:USERPROFILE\Documents\OpenRPA -itemtype directory
         while (!(Test-Path "C:\Program Files\OpenRPA\OpenRPA.exe")) { Start-Sleep 5 }
-        Set-CurrentUser -Save -Username $Env:USER1_AUTH_TOKEN -Password $Env:USER1_AUTH_TOKEN -WSURL ws://robot.shebeng.co
+        Set-CurrentUser -Save -Username $Env:USER1_AUTH_TOKEN -Password $Env:USER1_AUTH_TOKEN -WSURL wss://robot.shebeng.co
         Start-Process "OpenRPA.exe"     
         
 }
@@ -15,7 +15,7 @@ if( $Env:USER2_AUTH_TOKEN -eq $env:UserName) {
 
         new-item $env:USERPROFILE\Documents\OpenRPA -itemtype directory
         while (!(Test-Path "C:\Program Files\OpenRPA\OpenRPA.exe")) { Start-Sleep 5 }
-        Set-CurrentUser -Save -Username $Env:USER2_AUTH_TOKEN -Password $Env:USER2_AUTH_TOKEN -WSURL ws://robot.shebeng.co
+        Set-CurrentUser -Save -Username $Env:USER2_AUTH_TOKEN -Password $Env:USER2_AUTH_TOKEN -WSURL wss://robot.shebeng.co
         Start-Process "OpenRPA.exe"
         
 }
